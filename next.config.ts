7de4fcb9d45c7bc66909b7d3d0840369f91ee964
ilyess.torki.v <ilyess.torki.v@gmail.com>
2026-06-10 19:40:18 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // This tells Next.js to skip the ESLint check during the build process
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
  
 const withNextIntl = createNextIntlPlugin();
+ 
 export default withNextIntl(nextConfig);
